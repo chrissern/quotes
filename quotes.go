b354b688e22336fcedf06957ab2a709a127d75b2
Christoffer <89209063+chrissern@users.noreply.github.com>
2022-01-28 22:18:51 +0100

    Create quotes.go

---
+++ quotes.go
@@ -0,0 +1,19 @@
+package quotes
+
+import "rsc.io/quote"
+
+func GetGo() string {
+	return quote.Go()
+}
+
+func GetHello() string {
+	return quote.Hello()
+}
+
+func GetOpt() string {
+	return quote.Opt()
+}
+
+func GetGlass() string {
+	return quote.Glass()
+}
